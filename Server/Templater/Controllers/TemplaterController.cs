abc3ad12abf077568bdf1ac18011f90647d1c971
FrostyMemes <lager11197@yandex.ru>
2023-01-18 22:57:02 +0300

    initial commit

---
+++ Server/Templater/Controllers/TemplaterController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Templater.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class TemplaterController : ControllerBase
+{
+    private readonly ILogger<TemplaterController> _logger;
+
+    public TemplaterController(ILogger<TemplaterController> logger)
+    {
+        _logger = logger;
+    }
+    
+    [HttpGet(Name = "ParseMarkdown")]
+    public void Get()
+    {
+        
+    }
+}
