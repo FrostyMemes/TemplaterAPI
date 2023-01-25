69b07f344328e2c84900d5dc3856df72d5f6fa06
FrostyMemes <lager11197@yandex.ru>
2023-01-26 00:55:13 +0300

    feat: add route for markdown processing

--- Server/Templater/Controllers/TemplaterController.cs
+++ Server/Templater/Controllers/TemplaterController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Templater.Builder;
 
 namespace Templater.Controllers;
 
@@ -8,15 +9,27 @@ namespace Templater.Controllers;
 public class TemplaterController : ControllerBase
 {
     private readonly ILogger<TemplaterController> _logger;
+    private readonly TemplateParser _templateParser;
 
     public TemplaterController(ILogger<TemplaterController> logger)
     {
         _logger = logger;
+        _templateParser = new TemplateParser();
     }
     
-    [HttpGet(Name = "ParseMarkdown")]
-    public void Get()
+    [HttpGet(Name = "TemplateMarkdown")]
+    public IActionResult Get(string markdown)
     {
+        try
+        {
+            var template = _templateParser.Parse(markdown);
+            return Ok(template);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500);
+        }
+        
         
     }
 }
