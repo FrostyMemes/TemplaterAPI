35ef36622e8598fb3855c18a0d0324bea240e5d8
Anatoliy <lager11197@yandex.ru>
2023-01-27 15:06:11 +0300

    feat: add Redis and Template services

--- Server/Templater/Program.cs
+++ Server/Templater/Program.cs
@@ -1,9 +1,17 @@
+using StackExchange.Redis;
+using Templater.Services.MarkdownTemplateService;
+
 var builder = WebApplication.CreateBuilder(args);
+var redisConnection = builder.Configuration["Redis:RedisConnectionString"];
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<ITemplateParser, TemplateParser>();
+builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(redisConnection));
+builder.Services.AddHttpClient();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
