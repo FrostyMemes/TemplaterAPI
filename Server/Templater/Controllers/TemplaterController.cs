afbb4067d02398fa0b42f951ce0f1c83f9f38abc
FrostyMemes <lager11197@yandex.ru>
2023-02-21 01:58:30 +0300

    refactor: rewrite template controller

--- Server/Templater/Controllers/TemplaterController.cs
+++ Server/Templater/Controllers/TemplaterController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Runtime.InteropServices.JavaScript;
+using Microsoft.AspNetCore.Mvc;
 using StackExchange.Redis;
 using Templater.Services.MarkdownTemplateService;
 
@@ -9,16 +10,27 @@ namespace Templater.Controllers;
 public class TemplaterController : ControllerBase
 {
     private readonly ITemplateParser _templateParser;
+    private readonly ApplicationDbContext _dbContext;
 
-    public TemplaterController(ITemplateParser templateParser)
+    public TemplaterController(ITemplateParser templateParser, ApplicationDbContext dbContext)
     {
         _templateParser = templateParser;
+        _dbContext = dbContext;
     }
 
-    [HttpGet("{markdown}")]
+    [HttpGet]
     public async Task<string> Get(string markdown)
     {
         var result = await _templateParser.ParseAsync(markdown);
         return result;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult> Post(TemplateModel templateModel)
+    {
+        return Ok(new
+        {
+            Id = '1'
+        });
     }
 }
