6503b8f297028f8b744df9778f77e1f0ddd773d3
Anatoliy <lager11197@yandex.ru>
2023-02-24 01:07:17 +0300

    refactor: add null possible value for Id

--- Server/Templater/Models/TemplateModel.cs
+++ Server/Templater/Models/TemplateModel.cs
@@ -2,5 +2,8 @@
 
 public class TemplateModel
 {
-    
+    public string? Id { get; set; }
+    public string Title { get; set; }
+    public string Markdown { get; set; }
+    public string Markup { get; set; }
 }
