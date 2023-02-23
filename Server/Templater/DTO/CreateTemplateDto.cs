8caeddd41e71c891dc17877e635d9255690e4bee
Anatoliy <lager11197@yandex.ru>
2023-02-24 01:03:20 +0300

    feat: added DTO classes

---
+++ Server/Templater/DTO/CreateTemplateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Templater.DTO;
+
+public class CreateTemplateDto
+{
+    
+    public string Title { get; set; }
+    public string Markdown { get; set; }
+    public string Markup { get; set; }
+    public int UserId { get; set; }
+}
---
+++ Server/Templater/DTO/UpdateTemplateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Templater.DTO;
+
+public class UpdateTemplateDto
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Markdown { get; set; }
+    public string Markup { get; set; }
+}
