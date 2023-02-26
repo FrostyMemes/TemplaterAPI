1829d8aa218a813c561087ea49393a03fb983dac
FrostyMemes <lager11197@yandex.ru>
2023-02-26 23:14:53 +0300

    feat: add AnyHeader for CORS policy

--- Server/Templater/Program.cs
+++ Server/Templater/Program.cs
@@ -37,7 +37,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors(x => x
-    .AllowAnyOrigin());
+    .AllowAnyOrigin()
+    .AllowAnyHeader());
 
 app.UseAuthorization();
 
