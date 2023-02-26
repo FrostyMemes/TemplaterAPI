70f3b3969091a278c6f4dc522c0f7fa98ab92b97
FrostyMemes <lager11197@yandex.ru>
2023-02-26 23:14:00 +0300

    feat: add DTO for result for template query

---
+++ Server/Templater/DTO/ResultTemplateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Templater.DTO;
+
+public class ResultTemplateDto
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Markdown { get; set; }
+    public string Markup { get; set; }
+}
