ba0d72e4b0bdc9f841f910071e411f53e7ef6fa1
FrostyMemes <lager11197@yandex.ru>
2023-02-26 23:17:09 +0300

    refactor: clear code

--- Server/Templater/Models/TemplateModel.cs
+++ Server/Templater/Models/TemplateModel.cs
@@ -1,7 +1,7 @@
 ﻿namespace Templater.Models;
 
 public class TemplateModel
-{
+{ 
     public string? Id { get; set; }
     public string Title { get; set; }
     public string Markdown { get; set; }
