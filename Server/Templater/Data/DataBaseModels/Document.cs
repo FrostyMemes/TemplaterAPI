2e8b3b2b1646c918b8ea4b1ce6d1f3695a5d395d
FrostyMemes <lager11197@yandex.ru>
2023-05-23 01:23:51 +0300

    feat: added database table with documents

---
+++ Server/Templater/Data/DataBaseModels/Document.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Templater.Data.DataBaseModels;
+
+[Table("documents")]
+public class Document
+{
+    public Guid Id { get; set; }
+    public string FileName { get; set; }
+    
+    public int UserId { get; set; }
+    
+    public User User { get; set; }
+}
