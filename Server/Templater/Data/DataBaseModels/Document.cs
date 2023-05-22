9073e2682d8c0ef3f76fb409abd87559020afa30
FrostyMemes <lager11197@yandex.ru>
2023-05-23 01:54:16 +0300

    refactor: cleared code

--- Server/Templater/Data/DataBaseModels/Document.cs
+++ Server/Templater/Data/DataBaseModels/Document.cs
@@ -10,6 +10,6 @@ public class Document
     public string FileName { get; set; }
     
     public int UserId { get; set; }
-    
+
     public User User { get; set; }
 }
--- Server/Templater/Data/DataBaseModels/User.cs
+++ Server/Templater/Data/DataBaseModels/User.cs
@@ -7,10 +7,8 @@ namespace Templater.Data.DataBaseModels;
 public class User
 {
     public int Id { get; set; }
-    
     public string Username { get; set; }
     public string Email { get; set; }
-    
     public string HashedPassword  { get; set; }
     
     public ICollection<Template> Templates { get; set; }
