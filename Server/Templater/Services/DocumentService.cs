ddb10cbf52a6d8875a47c4d03eed9037d81c8a7f
FrostyMemes <lager11197@yandex.ru>
2023-05-24 01:08:43 +0300

    feat: added controller for working with documents

---
+++ Server/Templater/Services/DocumentService.cs
@@ -0,0 +1,49 @@
+﻿using Templater.DTO.DocumentDTO;
+using Templater.Services.Interfaces;
+
+namespace Templater.Services;
+
+public class DocumentService: IDocumentService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public DocumentService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<IEnumerable<AllDocumentsDto>> GetAllDocumentsAsync()
+    {
+        return await _dbContext.Documents
+            .Select(document => new AllDocumentsDto()
+            {
+                Id = document.Id.ToString(),
+                FileName = document.FileName,
+            })
+            .ToListAsync();
+    }
+
+    public async Task<GetDocumentDto> GetDocumentAsync(Guid id)
+    {
+        #pragma warning disable CS8603
+        return await _dbContext.Documents
+            .Where(document => document.Id == id)
+            .Select(document => new GetDocumentDto()
+            {
+                Id = document.Id.ToString(),
+                FileName = document.FileName,
+            })
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<Document> DeleteDocumentAsync(Guid id)
+    {
+        var document = await _dbContext.Documents.FindAsync(id);
+        if (document != null)
+        {
+            _dbContext.Documents.Remove(document);
+            await _dbContext.SaveChangesAsync();
+        }
+        return document;
+    }
+}
