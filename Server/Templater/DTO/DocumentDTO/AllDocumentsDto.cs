35ebca835d5563a89e07703fea49aebfd294db1c
FrostyMemes <lager11197@yandex.ru>
2023-05-24 01:07:29 +0300

    feat: added DTOs for documents

---
+++ Server/Templater/DTO/DocumentDTO/AllDocumentsDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Templater.DTO.DocumentDTO;
+
+public class AllDocumentsDto
+{
+    public string Id { get; set; }
+    public string FileName { get; set; }
+}
---
+++ Server/Templater/DTO/DocumentDTO/GetDocumentDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Templater.DTO.DocumentDTO;
+
+public class GetDocumentDto
+{
+    public string Id { get; set; }
+    public string FileName { get; set; }
+}
